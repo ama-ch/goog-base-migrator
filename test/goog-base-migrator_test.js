bf4f8e363d790d386982ac752e4a51c6ed484937
ama-ch <amach97@gmail.com>
2014-04-20 15:52:15 +0900

    Show debug log as necessary.

--- test/goog-base-migrator_test.js
+++ test/goog-base-migrator_test.js
@@ -2,13 +2,21 @@
 
 var assert = require("power-assert");
 var fs = require("fs");
-var migrator = require('../lib/goog-base-migrator.js');
+var Logger = require('../lib/logger');
+var Migrator = require('../lib/goog-base-migrator.js');
 
 describe('migrator', function() {
+  var migrator;
+
+  beforeEach(function() {
+    var logger = new Logger(process.stdout, process.stderr);
+    migrator = new Migrator(logger);
+  });
+
   context('basic', function() {
     it('migrate from goog.base to static base method.', function() {
       var src = fs.readFileSync('test/fixture/basic.js', 'utf8');
-      var actual = migrator(src);
+      var actual = migrator.migrate(src);
       var expected = fs.readFileSync('test/fixture/basic-fixed.js', 'utf8');
       assert.equal(actual, expected);
     });
